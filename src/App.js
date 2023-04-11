75da2358b642abd2fde80f277b83ad3a880c3efc
lilayfo <114691909+lilayfo@users.noreply.github.com>
2023-04-11 16:12:12 -0400

    tests passing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
+import './App.css';
+import axios from 'axios';
+import styled from 'styled-components';
+
+import Character from './components/Character';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+  const [stars, setStars] = useState(null);
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people/')
+    .then(res => {
+      setStars(res.data.results);
+      setStars(res.data);
+    })
+    .catch(err => {
+      console.error(err)
+    })
+  }, []);
+
+  const StarWarsDiv = styled.div
+  `
+  margin: 0 auto;
+  box-sizing: border-box;
+  width: 50%;
+`
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+        <h1 className="Header">Characters</h1>
+
+    <StarWarsDiv>
+
+    {
+      stars &&
+      <>
+      {
+        stars.map((character, index) => {
+          return <Character key={index} info={character}/>
+        })
+      }
+      </>
+    }
+    </StarWarsDiv>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,30 @@
 // Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+
+const StarStyle = styled.div
+`
+display:flex;
+justify-content:center;
+`
+
+const StarStyleBox = styled.div
+`
+margin:10% 15% 0;
+background-color:blue;
+border: solid black 5px;
+border-radius:10%
+`
+
+const Character = props => {
+    const {info} = props
+    return (
+        <StarStyle>
+
+            <h1>{ info.name }</h1>
+
+        </StarStyle>
+    )
+}
+
+export default Character;
